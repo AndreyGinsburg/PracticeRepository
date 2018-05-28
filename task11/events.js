9e984d0104f2639e8dacaed39bbed988fc61c419
AndreyGinsburg <andrey_ginsburg@mail.ru>
2018-05-28 19:38:40 +0300

    add task11

---
+++ task11/events.js
@@ -0,0 +1,413 @@
+let user;
+
+function toAuthorization() {
+    if (localStorage.getItem("maxID") === null) {
+        localStorage.setItem("maxID", "22");
+    }
+    if (localStorage.getItem("posts") === null) {
+        localStorage.setItem("posts", JSON.stringify(photoPosts));
+    }
+    let x = document.getElementsByClassName("button_first")[0];
+    x.addEventListener("click", (e) => {
+        let a = `
+            
+       <img class="picture1" src="author_tyan.png">
+       <div class="filters">
+           <input id="address" type="text" class=filter placeholder="Email Address">
+           <input id="password" type="text" class=filter placeholder="Password">
+       </div>
+       <button class="log-in">
+           Log in
+       </button>
+   
+       `
+        let element = document.getElementById("feed");
+        element.innerHTML = a;
+        a = `
+
+       <button class="btn btn-sign-out">
+            <img class="signout" src="signin.png" title="sign out">
+        </button>
+
+        <div class="Logo">
+            <img class="logo" src="logo_for_author.png" alt="Logo">
+        </div>
+
+        `
+        element = document.getElementsByClassName("header")[0];
+        element.innerHTML = a;
+        let y = document.getElementsByClassName("log-in")[0];
+        y.addEventListener("click", MainPage);
+    })
+}
+
+function MainPage() {
+    photoPosts = JSON.parse(localStorage.getItem("posts"));
+    let addr = document.getElementById("address");
+    if (addr) {
+        user = addr.value;
+    }
+    let a = `
+    <div class="filters-container">
+    <div class="add">
+        <img class="AddMio" src="Mio.png"title="Mio">
+
+        <button class="btn addphoto">
+             Add photo
+        </button>
+    </div>
+
+     <div class="filters">
+         <img class="Yui" src="Yui.png" title="Filters">
+         <input id=author type="text" class=filter placeholder="username">
+         <input id=hashtags type="text" class=filter placeholder="#hashtags">
+        <input  id=date type="text" class=filter  placeholder="dd.mm.yyyy">
+        <button class="find">Find</button>
+    </div>
+</div>
+
+<div class="feed" id="feed">
+
+</div>
+
+<button class="btn btn-load-more">
+load more
+</button>
+       `
+    let element = document.getElementsByClassName("main")[0];
+    element.innerHTML = a;
+    let b = `
+        <title>Ginpugramovich</title>
+        <link rel="shortcut icon" href="Без названия.jpg" type="image/x-icon">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="styles.css" rel="stylesheet">
+        <script src="index.js"></script>
+        <script src="script.js"></script>
+        <script src="events.js"></script>
+        `
+    element = document.getElementsByTagName("head")[0];
+    element.innerHTML = b;
+    b = `
+        <div class="Logo">
+        <img class="logo" src="logo.png" alt="Logo">
+    </div>
+
+    <div id="user-info">
+            <img class="user"src="15147547804090.png" title="Username">
+    </div>
+
+    <button class="btn btn-sign-out">
+        <img class="signout" src="011_Veselyiy_rodzher-550x550.png" title="sign out">
+    </button>
+        `
+    element = document.getElementsByClassName("header")[0];
+    element.innerHTML = b;
+    Username(user);
+    Clear();
+    showPosts();
+    let x = document.getElementsByClassName("btn btn-load-more")[0];
+    x.addEventListener("click", (e) => {
+        pressLoadMoreButton();
+        let Posts = document.getElementsByClassName("post");
+        for (post_t of Posts) {
+            var aut = post_t.getElementsByClassName("post-username")[0].id;
+            let red = post_t.getElementsByClassName("edit-icon material-icons")[0];
+            var dat = post_t.getElementsByClassName("post-upload-data")[0].id;
+            if (aut === user) {
+                let del = post_t.getElementsByClassName("delete-icon material-icons")[0];
+                let num = post_t.id;
+                del.addEventListener("click", (rem) => {
+                    removePhotoPost(num);
+                    localStorage.setItem("posts", JSON.stringify(photoPosts));
+                    MainPage();
+                });
+                red.addEventListener("click", (ee) => {
+                    Username(user);
+                    let a = `
+                <div class="feed">
+    
+        <img class="picture1" src="add1.png" >
+        
+        <div class="addchange">
+        <input id="link" type="text" class=date placeholder="link">
+        <input id="description" type="text" class=date placeholder="description">
+        <input  id="hashtags" type="text" class=date  placeholder="#hashtags">
+        <button class="addph">
+        Change
+        </button>
+        </div>
+        
+        <img class="picture2" src="add2.png">
+    
+    </div>
+    `
+                    let x = document.getElementsByClassName("main")[0];
+                    x.innerHTML = a;
+                    a = `
+    <title>Ginpugramovich:Edit</title>
+    <link rel="shortcut icon" href="Без названия.jpg" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="styles_for_add.css" rel="stylesheet">
+    <script src="index.js"></script>
+    <script src="script.js"></script>
+    <script src="events.js"></script>
+    `
+                    x = document.getElementsByTagName("head")[0];
+                    x.innerHTML = a;
+                    a = `
+    <div class="Logo">
+        <img class="logo" src="logo_for_add.png" alt="Logo">
+    </div>
+    
+    <div id="user-info">
+            <img class="user"src="15147547804090_for_add.png" title="Username">
+    </div>
+    
+    <button class="btn btn-sign-out">
+        <img class="signout" src="011_Veselyiy_rodzher-550x550.png" title="sign out">
+    </button>
+    `
+                    x = document.getElementsByClassName("header")[0];
+                    x.innerHTML = a;
+                    Username(user);
+                    x = document.getElementsByClassName("btn btn-sign-out")[0]
+                    x.addEventListener("click", Exit);
+                    x = document.getElementsByClassName("addph")[0];
+                    x.addEventListener("click", (e) => {
+                        let link = document.getElementById("link").value
+                        let description = document.getElementById("description").value
+                        let hashtags = document.getElementById("hashtags").value
+                        let tags = hashtags.split(',');
+                        let author = returnUser()
+                        let post1 = {
+                            id: num,
+                            description: description,
+                            createdAt: new Date(dat),
+                            author: user,
+                            photoLink: link,
+                            hashtags: [],
+                            likes: getPhotoPost(num).likes
+                        }
+                        k();
+                        removePhotoPost(num);
+                        addPhotoPost(post1);
+                        localStorage.setItem("posts", JSON.stringify(photoPosts));
+                        MainPage();
+                    })
+                })
+            }
+            let num = post_t.id;
+            let lucas = post_t.getElementsByClassName("like-icon material-icons")[0];
+        }
+    })
+    x = document.getElementsByClassName("btn btn-sign-out")[0];
+    x.addEventListener("click", Exit);
+    x = document.getElementsByClassName("find")[0];
+    x.addEventListener("click", find)
+    x = document.getElementsByClassName("btn addphoto")[0];
+    x.addEventListener("click", (e) => {
+        Username(user);
+        let a = `
+            <div class="feed">
+
+    <img class="picture1" src="add1.png" >
+    
+    <div class="addchange">
+    <input id="link" type="text" class=date placeholder="link">
+    <input id="description" type="text" class=date placeholder="description">
+    <input  id="hashtags" type="text" class=date  placeholder="#hashtags">
+    <button class="addph">
+    Add
+    </button>
+    </div>
+    
+    <img class="picture2" src="add2.png">
+
+</div>
+`
+        let x = document.getElementsByClassName("main")[0];
+        x.innerHTML = a;
+        a = `
+<title>Ginpugramovich:Add</title>
+<link rel="shortcut icon" href="Без названия.jpg" type="image/x-icon">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="styles_for_add.css" rel="stylesheet">
+<script src="index.js"></script>
+<script src="script.js"></script>
+<script src="events.js"></script>
+`
+        x = document.getElementsByTagName("head")[0];
+        x.innerHTML = a;
+        a = `
+<div class="Logo">
+    <img class="logo" src="logo_for_add.png" alt="Logo">
+</div>
+
+<div id="user-info">
+        <img class="user"src="15147547804090_for_add.png" title="Username">
+</div>
+
+<button class="btn btn-sign-out">
+    <img class="signout" src="011_Veselyiy_rodzher-550x550.png" title="sign out">
+</button>
+`
+        x = document.getElementsByClassName("header")[0];
+        x.innerHTML = a;
+        Username(user);
+        x = document.getElementsByClassName("btn btn-sign-out")[0]
+        x.addEventListener("click", Exit);
+        x = document.getElementsByClassName("addph")[0];
+        x.addEventListener("click", (e) => {
+            let link = document.getElementById("link").value
+            let description = document.getElementById("description").value
+            let hashtags = document.getElementById("hashtags").value
+            let tags = hashtags.split(' ');
+            let author = returnUser()
+            let post1 = {
+                id: '',
+                description: description,
+                createdAt: new Date(2026, 2, 5, 22, 11),
+                author: user,
+                photoLink: link,
+                hashtags: tags,
+                likes: [],
+            }
+            MainPage();
+            addPhotoPost(post1);
+            localStorage.setItem("posts", JSON.stringify(photoPosts));
+            MainPage();
+        })
+        localStorage.setItem("posts", JSON.stringify(photoPosts));
+
+    })
+    let Posts = document.getElementsByClassName("post");
+    for (post_t of Posts) {
+        var aut = post_t.getElementsByClassName("post-username")[0].id;
+        if (aut === user) {
+            let del = post_t.getElementsByClassName("delete-icon material-icons")[0];
+            let red = post_t.getElementsByClassName("edit-icon material-icons")[0];
+            let num = post_t.id;
+            del.addEventListener("click", (rem) => {
+                removePhotoPost(num);
+                localStorage.setItem("posts", JSON.stringify(photoPosts));
+                MainPage();
+            });
+            red.addEventListener("click", (ee) => {
+                Username(user);
+                let a = `
+            <div class="feed">
+
+    <img class="picture1" src="add1.png" >
+    
+    <div class="addchange">
+    <input id="link" type="text" class=date placeholder="link">
+    <input id="description" type="text" class=date placeholder="description">
+    <input  id="hashtags" type="text" class=date  placeholder="#hashtags">
+    <button class="addph">
+    Add
+    </button>
+    </div>
+    
+    <img class="picture2" src="add2.png">
+
+</div>
+`
+                let x = document.getElementsByClassName("main")[0];
+                x.innerHTML = a;
+                a = `
+<title>Ginpugramovich:Edit</title>
+<link rel="shortcut icon" href="Без названия.jpg" type="image/x-icon">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="styles_for_add.css" rel="stylesheet">
+<script src="index.js"></script>
+<script src="script.js"></script>
+<script src="events.js"></script>
+`
+                x = document.getElementsByTagName("head")[0];
+                x.innerHTML = a;
+                a = `
+<div class="Logo">
+    <img class="logo" src="logo_for_add.png" alt="Logo">
+</div>
+
+<div id="user-info">
+        <img class="user"src="15147547804090_for_add.png" title="Username">
+</div>
+
+<button class="btn btn-sign-out">
+    <img class="signout" src="011_Veselyiy_rodzher-550x550.png" title="sign out">
+</button>
+`
+                x = document.getElementsByClassName("header")[0];
+                x.innerHTML = a;
+                Username(user);
+                x = document.getElementsByClassName("btn btn-sign-out")[0]
+                x.addEventListener("click", Exit);
+                x = document.getElementsByClassName("addph")[0];
+                x.addEventListener("click", (e) => {
+                    let link = document.getElementById("link").value
+                    let description = document.getElementById("description").value
+                    let hashtags = document.getElementById("hashtags").value
+                    let tags = hashtags.split(',');
+                    let author = returnUser()
+                    let post1 = {
+                        id: num,
+                        description: description,
+                        createdAt: new Date(2026, 2, 5, 22, 11),
+                        author: user,
+                        photoLink: link,
+                        hashtags: tags,
+                        likes: []
+                    }
+                    k();
+                    removePhotoPost(num);
+                    addPhotoPost(post1);
+                    localStorage.setItem("posts", JSON.stringify(photoPosts));
+                    k();
+                })
+            })
+        }
+
+    }
+}
+
+function Exit() {
+    Clear();
+    let b = `
+            <div class="Logo">
+            <img class="logo" src="logo_for_author.png" alt="Logo">
+        </div>
+        `
+    element = document.getElementsByClassName("header")[0];
+    element.innerHTML = b;
+    b = `
+            <div class="Logo">
+            <img class="logo" src="logo_for_author.png" alt="Logo">
+        </div>
+        `
+    element = document.getElementsByClassName("header")[0];
+    element.innerHTML = b;
+    b = `
+        <title>Ginpugramovich</title>
+<link rel="shortcut icon" href="Без названия.jpg" type="image/x-icon">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="styles_for_author.css" rel="stylesheet">
+<script src="index.js"></script>
+<script src="script.js"></script>
+<script src="templates.js"></script>
+    `
+    element = document.getElementsByTagName("head")[0];
+    element.innerHTML = b;
+    b = `
+    <div class="feed" id="feed">
+    <button class="button_first">
+        Go to 
+        authorization
+    </button>
+    </div>
+`
+    element = document.getElementsByClassName("main")[0];
+    element.innerHTML = b;
+    toAuthorization();
+}
+window.addEventListener('load', toAuthorization)
---
+++ task11/index.js
@@ -0,0 +1,322 @@
+var photoPosts = [{
+    id: '1',
+    description: 'Второе место на РТЮМ',
+    createdAt: new Date(2018, 02, 19, 22, 41),
+    author: 'Ginpukisreal',
+    photoLink: 'http://www.uni.bsu.by/arrangements/turnir/foto2008/rtum2015_gomel_big.jpg',
+    hashtags: ['#RTYM', '#Gomel,'],
+    likes: ['boris_minsk', 'kukkel', 'dubovik_official']
+
+},
+
+{
+    id: '2',
+    description: 'Учусь вот',
+    createdAt: new Date(2018, 02, 19, 23, 12),
+    author: 'Akiyama_Mio',
+    photoLink: 'http://pm1.narvii.com/6720/ea5b1161f93ad90b0daaa7a6759ce6f5acace16a_00.jpg',
+    hashtags: ['#Родина', '#СлушаюХацунеМику,'],
+    likes: ['Ginpukisreal', 'Kurisu_Makise']
+},
+
+{
+    id: '3',
+    description: 'Лучший напиток после конференции',
+    createdAt: new Date(2018, 02, 19, 23, 12),
+    author: 'Kurisu_Makise',
+    photoLink: 'https://2ch.hk/b/arch/2016-03-28/src/121556262/14591321612550.jpg',
+    hashtags: ['#Dr.Pepper'],
+    likes: ['Ginpukisreal', 'VKoz', 'Okabe_Rintaro']
+},
+
+{
+    id: '4',
+    description: 'Лайк если любишь этот корпус',
+    createdAt: new Date(2018, 02, 19, 23, 18),
+    author: 'BSU',
+    photoLink: 'https://www.belnovosti.by/sites/default/files/styles/692/public/article/17-07-2017/bsu-student.jpg?itok=kAlbI9Jq',
+    hashtags: ['#ГлавныйКорпус'],
+    likes: []
+},
+
+{
+    id: '5',
+    description: 'Наконец-то успех',
+    createdAt: new Date(2018, 02, 19, 23, 18),
+    author: 'boris_minsk',
+    photoLink: 'http://quizup-players.imgix.net/players/1033889251093300208/pictures/aldv7u6sky/original.jpg',
+    hashtags: ['#IMCтакнерадует'],
+    likes: ['Ginpukisreal', 'VKoz', 'kukkel', 'Katarina1999']
+},
+
+{
+    id: '6',
+    description: 'Прекрасный город',
+    createdAt: new Date(2018, 02, 19, 23, 18),
+    author: 'Ginpukisreal',
+    photoLink: 'https://gdb.rferl.org/14AF7C89-419F-4EB1-87EC-FB88746217B6_w1023_r1_s.jpg',
+    hashtags: ['#Jerusalem'],
+    likes: ['Katarina1999', 'kukkel']
+},
+
+{
+    id: '7',
+    description: 'С лучшим другом',
+    createdAt: new Date(2018, 02, 19, 23, 18),
+    author: 'sONYA',
+    photoLink: 'https://www.gl5.ru/photos/f/fadeev-max/versus.jpg',
+    hashtags: ['#ОхладиПыл', '#Рилток'],
+    likes: ['dubovik_official']
+},
+
+{
+    id: '8',
+    description: 'Жалко что 3 не доделал',
+    createdAt: new Date(2018, 02, 19, 23, 18),
+    author: 'dubovik_official',
+    photoLink: 'https://s9.stc.all.kpcdn.net/share/i/12/10133290/cr-1200-628.vn-4-1411783-100-tl-0-0.t-13-3733742-ttps-50-12-32173D-1030-l-85-m-23.t-13-3733742-ttps-50-12-FFF-1030-l-85-m-22.t-207-5-ossb-25-10-FFF-788-l-115-t-47',
+    hashtags: ['#Бразилия', '#IMO', '#Gomel'],
+    likes: ['Ginpukisreal', 'VKoz', 'Katarina1999', 'boris_minsk', 'kukkel']
+},
+
+{
+    id: '9',
+    description: 'Лайк если любишь этот корпус',
+    createdAt: new Date(2018, 02, 20, 23, 18),
+    author: 'BSU',
+    photoLink: 'https://www.belnovosti.by/sites/default/files/styles/692/public/article/17-07-2017/bsu-student.jpg?itok=kAlbI9Jq',
+    hashtags: ['#ГлавныйКорпус'],
+    likes: []
+},
+
+{
+    id: '10',
+    description: 'Наконец-то успех',
+    createdAt: new Date(2018, 02, 20, 23, 18),
+    author: 'boris_minsk',
+    photoLink: 'http://quizup-players.imgix.net/players/1033889251093300208/pictures/aldv7u6sky/original.jpg',
+    hashtags: ['#IMCтакнерадует'],
+    likes: ['Ginpukisreal', 'VKoz', 'kukkel', 'Katarina1999']
+},
+
+{
+    id: '11',
+    description: 'Прекрасный город',
+    createdAt: new Date(2018, 02, 20, 23, 18),
+    author: 'Ginpukisreal',
+    photoLink: 'https://gdb.rferl.org/14AF7C89-419F-4EB1-87EC-FB88746217B6_w1023_r1_s.jpg',
+    hashtags: ['#Jerusalem'],
+    likes: ['Katarina1999', 'kukkel']
+},
+
+{
+    id: '12',
+    description: 'С лучшим другом',
+    createdAt: new Date(2018, 02, 20, 23, 18),
+    author: 'sONYA',
+    photoLink: 'https://www.gl5.ru/photos/f/fadeev-max/versus.jpg',
+    hashtags: ['#ОхладиПыл', '#Рилток'],
+    likes: ['dubovik_official']
+},
+
+{
+    id: '13',
+    description: 'Жалко что 3 не доделал',
+    createdAt: new Date(2018, 02, 20, 23, 18),
+    author: 'dubovik_official',
+    photoLink: 'https://s9.stc.all.kpcdn.net/share/i/12/10133290/cr-1200-628.vn-4-1411783-100-tl-0-0.t-13-3733742-ttps-50-12-32173D-1030-l-85-m-23.t-13-3733742-ttps-50-12-FFF-1030-l-85-m-22.t-207-5-ossb-25-10-FFF-788-l-115-t-47',
+    hashtags: ['#Бразилия', '#IMO', '#Gomel'],
+    likes: ['Ginpukisreal', 'VKoz', 'Katarina1999', 'boris_minsk', 'kukkel']
+},
+
+{
+    id: '14',
+    description: 'Учусь вот',
+    createdAt: new Date(2018, 02, 21, 23, 18),
+    author: 'Akiyama_Mio',
+    photoLink: 'http://pm1.narvii.com/6720/ea5b1161f93ad90b0daaa7a6759ce6f5acace16a_00.jpg',
+    hashtags: ['#Родина', '#СлушаюХацунеМику,'],
+    likes: ['Ginpukisreal', 'Kurisu_Makise']
+},
+
+{
+    id: '15',
+    description: 'Лучший напиток после конференции',
+    createdAt: new Date(2018, 02, 22, 23, 18),
+    author: 'Kurisu_Makise',
+    photoLink: 'https://2ch.hk/b/arch/2016-03-28/src/121556262/14591321612550.jpg',
+    hashtags: ['#Dr.Pepper'],
+    likes: ['Ginpukisreal', 'VKoz', 'Okabe_Rintaro']
+},
+
+{
+    id: '16',
+    description: 'Лайк если любишь этот корпус',
+    createdAt: new Date(2018, 02, 23, 23, 18),
+    author: 'BSU',
+    photoLink: 'https://www.belnovosti.by/sites/default/files/styles/692/public/article/17-07-2017/bsu-student.jpg?itok=kAlbI9Jq',
+    hashtags: ['#ГлавныйКорпус'],
+    likes: []
+},
+
+{
+    id: '17',
+    description: 'Наконец-то успех',
+    createdAt: new Date(2018, 02, 23, 23, 18),
+    author: 'boris_minsk',
+    photoLink: 'http://quizup-players.imgix.net/players/1033889251093300208/pictures/aldv7u6sky/original.jpg',
+    hashtags: ['#IMCтакнерадует'],
+    likes: ['Ginpukisreal', 'VKoz', 'kukkel', 'Katarina1999']
+},
+
+{
+    id: '18',
+    description: 'Прекрасный город',
+    createdAt: new Date(2018, 02, 23, 23, 18),
+    author: 'Ginpukisreal',
+    photoLink: 'https://gdb.rferl.org/14AF7C89-419F-4EB1-87EC-FB88746217B6_w1023_r1_s.jpg',
+    hashtags: ['#Jerusalem'],
+    likes: ['Katarina1999', 'kukkel']
+},
+
+{
+    id: '19',
+    description: 'С лучшим другом',
+    createdAt: new Date(2018, 02, 23, 23, 18),
+    author: 'sONYA',
+    photoLink: 'https://www.gl5.ru/photos/f/fadeev-max/versus.jpg',
+    hashtags: ['#ОхладиПыл', '#Рилток'],
+    likes: ['dubovik_official']
+},
+
+{
+    id: '20',
+    description: 'Жалко что 3 не доделал',
+    createdAt: new Date(2018, 02, 23, 23, 18),
+    author: 'dubovik_official',
+    photoLink: 'https://s9.stc.all.kpcdn.net/share/i/12/10133290/cr-1200-628.vn-4-1411783-100-tl-0-0.t-13-3733742-ttps-50-12-32173D-1030-l-85-m-23.t-13-3733742-ttps-50-12-FFF-1030-l-85-m-22.t-207-5-ossb-25-10-FFF-788-l-115-t-47',
+    hashtags: ['#Бразилия', '#IMO', '#Gomel'],
+    likes: ['Ginpukisreal', 'VKoz', 'Katarina1999', 'boris_minsk', 'kukkel']
+},
+]
+
+var functions = (function() {
+
+    let maxID = 22;
+
+    return {
+
+        isText(testText) {
+            return (typeof testText === 'string' && testText.length !== 0);
+        },
+
+        isDate(testDate) {
+            return (testDate instanceof Date);
+        },
+
+        isNumber(testNumber) {
+            return (typeof testNumber === 'number');
+        },
+
+        getPhotoPosts(skip = 0, top = 10, filter) {
+            if (!this.isNumber(skip) || !this.isNumber(top)) {
+                return false;
+            }
+            if (typeof filter === 'object') {
+                let filteredPosts = photoPosts.filter((photoPost) => {
+                    if (filter.hasOwnProperty('author') && (filter.author !== photoPost.author)) {
+                        return false;
+                    }
+                    if (filter.hasOwnProperty('Date') && (Date.parse(filter.Date) !== Date.parse(photoPost.createdAt))) {
+                        return false;
+                    }
+                    if (filter.hasOwnProperty('hashtags') && !filter.hashtags.every((tag) => {
+                            return photoPost.hashtags.includes(tag);
+                        })) {
+                        return false;
+                    }
+                });
+                return filteredPosts.slice(skip, top + skip);
+            } else {
+                if (filter == null) {
+                    return photoPosts.slice(skip, top + skip);
+                }
+            }
+        },
+
+        getPhotoPost(id) {
+            if (!this.isText(id)) {
+                return false;
+            }
+            for (var i = 0; i < photoPosts.length; ++i) {
+                if (photoPosts[i].id === id) {
+                    return photoPosts[i];
+                }
+            }
+            return 'There is no such id'
+        },
+
+        validatePhotoPost(post) {
+            return (typeof post === 'object' && this.isText(post.description) &&
+                this.isDate(post.createdAt) && this.isText(post.author) &&
+                this.isText(post.photoLink) && Array.isArray(post.hashtags) &&
+                Array.isArray(post.likes));
+        },
+
+        addPhotoPost(post) {
+            if (!this.validatePhotoPost(post)) {
+                return false;
+            }
+            photoPosts.push(post);
+            photoPosts.id = maxID + "";
+            maxID++;
+            photoPosts.sort((post1, post2) => {
+                return post2.createdAt - post1.createdAt;
+            });
+            return true;
+        },
+
+        editPhotoPost(id, editPost) {
+            if (!this.isText(id) || typeof editPost !== 'object') {
+                return false;
+            }
+            let post = this.getPhotoPost(id);
+            if (typeof post !== 'object') {
+                return false;
+            }
+            if (editPost.hasOwnProperty('description')) {
+                post.description = editPost.description;
+            }
+
+            if (editPost.hasOwnProperty('photoLink')) {
+                post.photoLink = editPost.photoLink;
+            }
+
+            if (editPost.hasOwnProperty('hashtags')) {
+                post.hashtags = editPost.hashtags;
+            }
+            if (editPost.hasOwnProperty('like')) {
+                post.like = editPost.like;
+            }
+            return true;
+        },
+
+        removePhotoPost(id) {
+            if (!this.isText(id)) {
+                return false;
+            }
+            for (var i = 0; i < photoPosts.length; ++i) {
+                if (photoPosts[i].id === id) {
+                    photoPosts.splice(i, 1);
+                    return true;
+                }
+            }
+            return false;
+        },
+
+    }
+})();
+
+for (method in functions) {
+    photoPosts.__proto__[method] = functions[method];
+}
